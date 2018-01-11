58391a343479771961447e1e79a8ae958277d0cd
andela-akhenda <joseph.akhenda@andela.com>
2018-01-11 17:48:56 +0300

    feat: create app theme

---
+++ src/theme/Colors.js
@@ -0,0 +1,32 @@
+// generated on https://www.canva.com/color-palette
+// from logo
+const lightSalmon = '#FCC46B';
+const sandyBrown = '#F48B43';
+const crimson = '#F4242C';
+// from splash screen
+const midnightBlue = '#204079';
+const darkSlateBlue = '#385C94';
+const lightBlue = '#F6F7F9';
+const cadetBlue1 = '#6783AE';
+const cadetBlue2 = '#8398BD';
+
+// create specific brand theme colors here. use https://material.io/color/
+const colors = {
+  primary: {
+    main: darkSlateBlue,
+    light: cadetBlue1,
+    lighter: cadetBlue2,
+    lightest: lightBlue,
+    dark: midnightBlue,
+    darker: '#001A4C',
+    text: '#FFFFFF',
+  },
+  secondary: {
+    main: sandyBrown,
+    light: lightSalmon,
+    dark: crimson,
+    text: '#000000',
+  },
+};
+
+export default colors;
---
+++ src/theme/Images.js
@@ -0,0 +1,8 @@
+const images = {
+  logo: require('../images/logo.png'),
+  backgroundPlain: require('../images/backround-plain.jpg'),
+  background75: require('../images/backround-75-opacity.jpg'),
+  background90: require('../images/backround-90-opacity.jpg'),
+};
+
+export default images;
