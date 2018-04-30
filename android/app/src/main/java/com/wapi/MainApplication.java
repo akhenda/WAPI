b71f0963dd9a6f503266cb94663ae277a4b8c5fb
akhenda <joseph.akhenda@andela.com>
2018-04-30 13:48:37 +0300

    feat(build): install react native exception handler

--- android/app/src/main/java/com/wapi/MainApplication.java
+++ android/app/src/main/java/com/wapi/MainApplication.java
@@ -3,6 +3,8 @@ package com.wapi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.avishayil.rnrestart.ReactNativeRestartPackage;
+import com.masteratul.exceptionhandler.ReactNativeExceptionHandlerPackage;
 import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.onradar.sdk.Radar;
 import com.onradar.react.RNRadarPackage;
@@ -29,6 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new ReactNativeRestartPackage(),
+          new ReactNativeExceptionHandlerPackage(),
           new LocationServicesDialogBoxPackage(),
           new LinearGradientPackage(),
           new ReactNativeConfigPackage(),
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import 'src/config';
 import React, { Component } from 'react';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, BackHandler } from 'react-native';
 import { Provider } from 'react-redux';
 import RequiresConnection from 'react-native-offline-mode';
 import { PersistGate } from 'redux-persist/es/integration/react';
+import { setJSExceptionHandler, setNativeExceptionHandler } from 'react-native-exception-handler';
 
 import { store, persistor } from 'src/state';
 import { colors } from './theme';
@@ -12,6 +13,36 @@ import Offline from './components/Offline';
 import RootContainer from './containers/RootContainer';
 import LoadingIndicator from './components/LoadingIndicator';
 
+
+const errorHandler = (e, isFatal) => {
+  if (isFatal) {
+    Alert.alert(
+        'Unexpected error occurred',
+        `
+        Error: ${(isFatal) ? 'Fatal:' : ''} ${e.name} ${e.message}
+        We have reported this to our team ! Please close the app and start again!
+        `,
+      [
+        {
+          text: 'Restart',
+          onPress: () => RNRestart.Restart(),
+        },
+        {
+          text: 'Quit',
+          onPress: () => BackHandler.exitApp(),
+        }
+      ]
+    );
+  } else {
+    if (__DEV__) console.log(e); // So that we can see it in the ADB logs in case of Android if needed
+  }
+};
+
+setJSExceptionHandler(errorHandler, true);
+
+setNativeExceptionHandler((errorString) => {
+  if (__DEV__) console.log('setNativeExceptionHandler');
+});
 
 class App extends Component {
   onBeforeLift = () => {
