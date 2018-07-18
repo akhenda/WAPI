2942750cc2292d5e5d06a3ff6bdba97d48fd95ec
akhenda <joseph.akhenda@andela.com>
2018-07-18 16:27:38 +0300

    fix(build): iOS build after RN upgrade

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,9 @@
-import { Text } from 'react-native';
 import debugConfig from './debug';
-import appConfig from './app';
 import './sentry';
 
 // Allow/disallow font-scaling in app
-Text.defaultProps.allowFontScaling = appConfig.allowTextFontScaling;
+// DISABLE: does not work in RN 0.56.0
+// Text.defaultProps.allowFontScaling = appConfig.allowTextFontScaling;
 
 if (__DEV__) {
   // If ReactNative's yellow box warnings are too much, it is possible to turn
